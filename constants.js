4a883c6712d5a67c6b5df575cd560755ea5bc9c0
Danielle Cruz <dcruz21@stanford.edu>
2021-12-09 23:22:17 -0800

    Added interactivivty to apologies edge bundles

--- constants.js
+++ constants.js
@@ -1,1 +1,4 @@
-COLORS = ["#021E73", "#F7F6F4", "#0176F2", "#FFD45A", "#EA0406"]
+const COLORS = ["#021E73", "#F7F6F4", "#0176F2", "#FFD45A", "#EA0406"];
+const COLOR_IN = "rgba(4, 0, 240, 0.6)";
+const COLOR_OUT = "rgba(240, 0, 0, 0.6)";
+const COLOR_NONE = "#ccc";
